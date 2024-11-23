44132761cca35bba1bf66045d8f89a1e987d0e09
Bojan Ðurković <b.durkovic@inshared.nl>
2024-11-23 21:18:57 +0100

    first commit

---
+++ vendor/_laravel_idea/_ide_helper_models_fcbef240bab02b28bcdf663e7b60a935.php
@@ -0,0 +1,48 @@
+<?php //e97d7fc59258325fa0c225cd668a573c
+/** @noinspection all */
+
+namespace App\Models {
+
+    use Database\Factories\UserFactory;
+    use Illuminate\Database\Eloquent\Model;
+    use Illuminate\Database\Eloquent\Relations\MorphToMany;
+    use Illuminate\Notifications\DatabaseNotification;
+    use Illuminate\Notifications\DatabaseNotificationCollection;
+    use Illuminate\Support\Carbon;
+    use LaravelIdea\Helper\App\Models\_IH_User_C;
+    use LaravelIdea\Helper\App\Models\_IH_User_QB;
+    use LaravelIdea\Helper\Illuminate\Notifications\_IH_DatabaseNotification_QB;
+
+    /**
+     * @property int $id
+     * @property string $name
+     * @property string $email
+     * @property Carbon|null $email_verified_at
+     * @property mixed $password
+     * @property string|null $remember_token
+     * @property Carbon|null $created_at
+     * @property Carbon|null $updated_at
+     * @property DatabaseNotificationCollection|DatabaseNotification[] $notifications
+     * @property-read int $notifications_count
+     * @method MorphToMany|_IH_DatabaseNotification_QB notifications()
+     * @property DatabaseNotificationCollection|DatabaseNotification[] $readNotifications
+     * @property-read int $read_notifications_count
+     * @method MorphToMany|_IH_DatabaseNotification_QB readNotifications()
+     * @property DatabaseNotificationCollection|DatabaseNotification[] $unreadNotifications
+     * @property-read int $unread_notifications_count
+     * @method MorphToMany|_IH_DatabaseNotification_QB unreadNotifications()
+     * @method static _IH_User_QB onWriteConnection()
+     * @method _IH_User_QB newQuery()
+     * @method static _IH_User_QB on(null|string $connection = null)
+     * @method static _IH_User_QB query()
+     * @method static _IH_User_QB with(array|string $relations)
+     * @method _IH_User_QB newModelQuery()
+     * @method false|int increment(string $column, float|int $amount = 1, array $extra = [])
+     * @method false|int decrement(string $column, float|int $amount = 1, array $extra = [])
+     * @method static _IH_User_C|User[] all()
+     * @foreignLinks
+     * @mixin _IH_User_QB
+     * @method static UserFactory factory(callable|int|mixed[]|null $count = null, callable|mixed[] $state = [])
+     */
+    class User extends Model {}
+}
