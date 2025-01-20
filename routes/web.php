fa1603c6678621266f6c866258148d4daa47d5f8
Bojan Ðurković <b.durkovic@inshared.nl>
2025-01-20 09:54:39 +0100

    during day 24 (Laravel in 30 days)

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,14 @@ use App\Http\Controllers\RegisteredUserController;
 use App\Http\Controllers\SessionController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('test', function () {
+    \Illuminate\Support\Facades\Mail::to('b.durkovic@inshared.nl')->send(
+        new \App\Mail\JobPosted()
+    );
+
+    return 'done';
+});
+
 Route::view('/', 'home');
 Route::view('/contact', 'contact');
 
