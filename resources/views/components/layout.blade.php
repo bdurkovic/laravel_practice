d7488dbb410affa8361a972259324eb26c4924de
Bojan Đurković <b.durkovic@inshared.nl>
2025-01-23 08:49:57 +0000

    end of day 26

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>My Website</title>
-    <script src="https://cdn.tailwindcss.com"></script>
+    @vite(['resources/js/app.js'])
 </head>
 <body class="h-full">
 
